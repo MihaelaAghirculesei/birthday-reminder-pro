bb995c520dce09850df89c5706f93198c5850fc1
MihaelaAghirculesei <aghirculesei@gmail.com>
2026-02-07 13:15:27 +0100

    ci: add Firebase Hosting deploy/preview jobs, add auth+sync to AppState

--- src/app/core/store/app.state.ts
+++ src/app/core/store/app.state.ts
@@ -2,10 +2,14 @@ import { BirthdayState } from './birthday/birthday.state';
 import { UIState } from './ui/ui.state';
 import { CalendarState } from './calendar/calendar.state';
 import { CategoryState } from './category/category.state';
+import { AuthState } from './auth/auth.state';
+import { SyncStatus } from './sync/sync.state';
 
 export interface AppState {
   birthdays: BirthdayState;
   ui: UIState;
   calendar: CalendarState;
   categories: CategoryState;
+  auth: AuthState;
+  sync: SyncStatus;
 }
