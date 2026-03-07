4e1452a6a928ea9a7cfeafcaad74c19ecd929430
MihaelaAghirculesei <aghirculesei@gmail.com>
2026-03-07 13:45:18 +0100

    refactor: remove unused calendar store module

--- src/app/core/store/app.state.ts
+++ src/app/core/store/app.state.ts
@@ -1,6 +1,5 @@
 import { BirthdayState } from './birthday/birthday.state';
 import { UIState } from './ui/ui.state';
-import { CalendarState } from './calendar/calendar.state';
 import { CategoryState } from './category/category.state';
 import { AuthState } from './auth/auth.state';
 import { SyncStatus } from './sync/sync.state';
@@ -8,7 +7,6 @@ import { SyncStatus } from './sync/sync.state';
 export interface AppState {
   birthdays: BirthdayState;
   ui: UIState;
-  calendar: CalendarState;
   categories: CategoryState;
   auth: AuthState;
   sync: SyncStatus;
