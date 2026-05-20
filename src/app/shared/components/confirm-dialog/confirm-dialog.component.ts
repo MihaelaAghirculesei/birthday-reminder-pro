5519a8ea377594f87fd3e28c8c434dbfea223435
MihaelaAghirculesei <aghirculesei@gmail.com>
2026-05-20 10:58:38 +0200

    refactor(confirm-dialog): replace @Inject constructor with inject()

--- src/app/shared/components/confirm-dialog/confirm-dialog.component.ts
+++ src/app/shared/components/confirm-dialog/confirm-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef, MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -204,10 +204,9 @@ export interface ConfirmDialogData {
   `]
 })
 export class ConfirmDialogComponent {
-  constructor(
-    public dialogRef: MatDialogRef<ConfirmDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: ConfirmDialogData
-  ) {}
+  dialogRef = inject<MatDialogRef<ConfirmDialogComponent>>(MatDialogRef);
+  data = inject<ConfirmDialogData>(MAT_DIALOG_DATA);
+
 
   onCancel(): void {
     this.dialogRef.close(false);
